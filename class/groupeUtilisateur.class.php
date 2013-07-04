1d6c44b8f70bd83d380c1196f665e39cac03ae35
mathieu <mathieu@mathieu-U36SD>
2013-07-04 17:43:13 +0200

    Classe GroupeUtilisateur terminée (code et phpDoc)

--- class/groupeUtilisateur.class.php
+++ class/groupeUtilisateur.class.php
@@ -2,24 +2,55 @@
 
 class GroupeUtilisateur 
 {
+    /**
+    * Id unique du groupe
+    * @var int $m_idGroupe
+    */
     private $m_idGroupe;
+    /**
+    * Libelle du groupe
+    * @var string $m_libelleGroupe
+    */
     private $m_libelleGroupe;
+    /**
+    * Base de données
+    * @var BaseDeDonnees $m_bdd
+    */
     private $m_bdd;
     
     /* ------------------------------------------------- Getteurs ------------------------------------------------- */
-    
+    /**
+    * Getteur de l'idGroupe
+    * @return int $this->m_idGroupe valeur de l'id du groupe
+    */
     public function getIdGroupe()
     {
         return $this->m_idGroupe;
     }
-    
+    /**
+    * Getteur du libelleGroupe
+    * @return int $this->libelleGroupe valeur du libelle du groupe
+    */
     public function getLibelleGroupe()
     {
         return $this->m_libelleGroupe;
     }
     
+    /* ------------------------------------------------- Setteurs ------------------------------------------------- */
+    /**
+    * Setteur du libelle du groupe
+    * @param string $libelleGroupe Nouveau libelle du groupe
+    */
+    public function setLibelleGroupe($libelleGroupe)
+    {
+        $this->m_libelleGroupe = $libelleGroupe;
+    }
+    
     /* ------------------------------------------------- Constructeurs ------------------------------------------------- */
-    // Construction du groupe grâce à l'Id via la base de données
+    /**
+     * Construction du groupe grâce à l'Id via la base de données
+     * @param int $idGroupe $idGroupe Id du groupe à aller chercher dans la base de données
+     */
     private function constructId($idGroupe)
     {
         $this->m_idGroupe = $idGroupe;
@@ -37,7 +68,11 @@ class GroupeUtilisateur
         $this->m_libelleGroupe = $resultat[0]['libelleGroupe'];   
     }
     
-    // Construction d'un nouveau groupe grâce aux informations fournies
+    /**
+     * Construction d'un nouveau groupe grâce aux informations fournies
+     * 
+     * @param string $libelleGroupe Libelle du nouveau groupe
+     */
     private function constructNew($libelleGroupe)
     {
         $this->m_libelleGroupe = $libelleGroupe;
@@ -62,6 +97,88 @@ class GroupeUtilisateur
         {
             $this->constructNew($arg_list[0]);
         }        
+    }
+    
+    /* ------------------------------------------------- Gestion base de données ------------------------------------------------- */
+    
+    /**
+     * Ajoute le groupe dans la base de données
+     * @return boolean Indiquant si l'ajout a bien été effectué ou pas
+     */
+    public function ajouterGroupe()
+    {
+        if(!$this->existeDeja())
+        {
+            $requete = 'INSERT INTO groupeUtilisateur (libelleGroupe)
+                                VALUES (?)';
+            $tabParametres = array($this->m_libelleGroupe);
+
+            $resultat = $this->m_bdd->ajouter($requete, $tabParametres);
+
+            return $resultat;
+        }
+        else
+            return FALSE;
+    }
+    
+    /**
+     * Modification du groupe dans la base de données
+     * @return boolean Indiquant si l'ajout a bien été effectué ou pas
+     */
+    public function modifierGroupe()
+    {
+        if($this->existeDeja())
+        {
+            $requete = 'UPDATE groupeUtilisateur
+                                SET (libelleGroupe = ?)
+                                WHERE idGroupe = ?)';
+            $tabParametres = array($this->m_libelleGroupe, $this->m_idGroupe);
+
+            $resultat = $this->m_bdd->modifier($requete, $tabParametres);
+
+            return $resultat;
+        }
+        else
+            return FALSE;
+    }
+    
+    /**
+     * Suppression du groupe dans la base de données
+     * @return boolean Indiquant si l'ajout a bien été effectué ou pas
+     */
+    public function supprimerGroupe()
+    {
+        if($this->existeDeja())
+        {
+            $requete = 'DELETE FROM groupeUtilisateur
+                                WHERE idGroupe = ?';
+            $tabParametres = array($this->m_idGroupe);
+
+            $resultat = $this->m_bdd->supprimer($requete, $tabParametres);
+
+            return $resultat;
+        }
+        else
+            return FALSE;
+    }
+    
+    /**
+     * Fonction qui vérifie si le groupe existe déjà dans la base de données
+     * @return boolean Indiquant si le groupe existe ou pas
+     */
+    function existeDeja()
+    {
+        $requete = 'SELECT *
+                            FROM groupeUtilisateur
+                            WHERE idGroupe = ? AND libelleGroupe = ?';
+        $tabParametres = array($this->m_idGroupe, $this->m_libelleGroupe);
+        
+        $resultat = $this->m_bdd->selection($requete, $tabParametres);
+        
+        if(empty($resultat))
+            return TRUE;
+        else
+            return FALSE;
     }
 }
 
