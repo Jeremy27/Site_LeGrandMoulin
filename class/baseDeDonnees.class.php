ebb488109a743d2b60d25e089a4204506a6d719c
Jeremy27 <courel.jeremy@gmail.com>
2013-07-04 14:47:36 +0200

    ajout des commentaires

--- class/baseDeDonnees.class.php
+++ class/baseDeDonnees.class.php
@@ -1,16 +1,6 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of baseDeDonnees
- *
- * @author mathieu
- */
-class baseDeDonnees
+class BaseDeDonnees
 {
     private $m_nomBdd           = 'LeGrandMoulin';
     private $m_hoteBdd          = 'localhost';
@@ -19,6 +9,10 @@ class baseDeDonnees
     private $m_connexion;
     private $m_email = 'mathieu.courel@gmail.com, courel.jeremy@gmail.com';
     
+    /**
+     * Création de la connexion
+     * Si une erreur survient alors un mail sera envoyé aux deux admin
+     */
     public function __construct()
     {
         try  
@@ -28,20 +22,26 @@ class baseDeDonnees
         } 
         catch(PDOException $e) 
         { 
-            echo 'catch';
-            //On indique par email qu'on n'a plus de connection disponible 
             mail($this->m_email, 'Urgent: Problème de connexion à la Base de données', date('D/m/y').' à '.date("H:i:s").' : '.$e->getMessage());
         } 
     }
     
-    // Renvoie le dns permettant la connexion à la base de données
+    /**
+     * @return String : dns permettant la connexion à la base de données
+     */
     private function getDns()
     {
         $dns = "mysql:dbname=$this->m_nomBdd;host=$this->m_hoteBdd";
         return $dns;
     }
     
-    /* ------------------------------------------------- Fonctions permettants le select, l'update et le delete ------------------------------------------------- */
+    /**
+     * Cette méthode prend en paramètres la requête à executer et le tableau de paramètre pour la requête préparée, elle est utilisée pour 
+     * lire des données dans la base de donnnées
+     * @param String $requete : ce paramètre correspond à la requête au format préparé (contenant donc des `?`)
+     * @param array $tabParametres : ce paramètre correspond aux valeur à insérer dans la requête préparée
+     * @return array : un tableau multidimensionnel contenant les resultats de la requete
+     */
     public function selection($requete, $tabParametres)
     {
         $requetePreparee = $this->m_connexion->prepare($requete);
@@ -53,20 +53,41 @@ class baseDeDonnees
         
         return $tableau;
     }
+  
+    /**
+     * Cette méthode prend en paramètres la requête à executer et le tableau de paramètre pour la requête préparée, elle est utilisée pour 
+     * ajouter un élément déjà existant dans la bdd
+     * @param String $requete : ce paramètre correspond à la requête au format préparé (contenant donc des `?`)
+     * @param array $tabParametres : ce paramètre correspond aux valeur à insérer dans la requête préparée
+     * @return boolean : retourne TRUE ou FALSE en fonction de si la requete s'est bien passée ou pas
+     */
+    public function ajouter($requete, $tabParametres)
+    {
+        $requetePreparee = $this->m_connexion->prepare($requete);
+        return $requetePreparee->execute($tabParametres);
+    }
     
+    /**
+     * Cette méthode prend en paramètres la requête à executer et le tableau de paramètre pour la requête préparée, elle est utilisée pour 
+     * modifier un élément déjà existant dans la bdd
+     * @param String $requete : ce paramètre correspond à la requête au format préparé (contenant donc des `?`)
+     * @param array $tabParametres : ce paramètre correspond aux valeur à insérer dans la requête préparée
+     * @return boolean : retourne TRUE ou FALSE en fonction de si la requete s'est bien passée ou pas
+     */
     public function modifier($requete, $tabParametres)
     {
         $requetePreparee = $this->m_connexion->prepare($requete);
         return $requetePreparee->execute($tabParametres);
     }
   
+    /**
+     * Cette méthode prend en paramètres la requête à executer et le tableau de paramètre pour la requête préparée, elle est utilisée pour 
+     * supprimer un élément existant dans la bdd
+     * @param String $requete : ce paramètre correspond à la requête au format préparé (contenant donc des `?`)
+     * @param array $tabParametres : ce paramètre correspond aux valeur à insérer dans la requête préparée
+     * @return boolean : retourne TRUE ou FALSE en fonction de si la requete s'est bien passée ou pas
+     */
     public function supprimer($requete, $tabParametres)
-    {
-        $requetePreparee = $this->m_connexion->prepare($requete);
-        return $requetePreparee->execute($tabParametres);
-    }
-  
-    public function ajouter($requete, $tabParametres)
     {
         $requetePreparee = $this->m_connexion->prepare($requete);
         return $requetePreparee->execute($tabParametres);
